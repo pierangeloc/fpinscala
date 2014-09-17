c45685cb03a04441ee8a3b632774898afe7c53ec
pierangeloc <pierangeloc@gmail.com>
2014-09-17 08:02:32 +0200

    Lists + attempt to include testing frameworks

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -4,16 +4,38 @@ package fpinscala.errorhandling
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = sys.error("todo")
 
-  def getOrElse[B>:A](default: => B): B = sys.error("todo")
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(x) => Some(f(x))
+    case None => None
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = sys.error("todo")
+  def getOrElse[B>:A](default: => B): B = this match {
+    case Some(x) => x
+    case None => default
+  }
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = sys.error("todo")
+  def flatMap[B](f: A => Option[B]): Option[B] = map(f).getOrElse(None)
+//  = this match {
+//    case Some(x) => f(x)
+//    case None => None
+//  }
 
-  def filter(f: A => Boolean): Option[A] = sys.error("todo")
+  //todo: impl without patmat?
+  def orElse[B>:A](ob: => Option[B]): Option[B] = map(Some(_)).getOrElse(ob)
+//  this match {
+//    case None => ob
+//    case _ => _
+//  }
+
+  //todo: impl without patmat?
+  def filter(f: A => Boolean): Option[A] = flatMap((x:A) => if (f(x)) Some(x) else None)
+//    this match {
+//    case Some(x) if f(x) => Some(x)
+//    case _ => None
+//  }
 }
+
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
@@ -38,11 +60,20 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = sys.error("todo")
+
+  def variance(xs: Seq[Double]): Option[Double] = mean(xs)map((mean: Double) => xs.map((el: Double) => Math.pow((el - mean), 2)).sum / xs.length)
 
   def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
 
   def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = sys.error("todo")
+}
+
+object TestOption {
+  def main(args: Array[String]): Unit = {
+    val values = List(1.0, 2.0, 3.0, 4.0,5.0)
+    println("Testing variance on Sequence: " + values + ": var = " + Option.variance(values))
+  }
+
 }
