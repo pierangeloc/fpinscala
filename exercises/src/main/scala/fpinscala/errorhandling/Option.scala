3adff0e9f7571bdabed5160de08a82c5f9ebb3e2
pierangeloc <pierangeloc@gmail.com>
2014-09-20 17:41:40 +0200

    variance exercise

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -3,6 +3,7 @@ package fpinscala.errorhandling
 
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
+
 sealed trait Option[+A] {
 
   def map[B](f: A => B): Option[B] = this match {
@@ -40,6 +41,7 @@ case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
 object Option {
+  import math.pow
   def failingFn(i: Int): Int = {
     val y: Int = throw new Exception("fail!") // `val y: Int = ...` declares `y` as having type `Int`, and sets it equal to the right hand side of the `=`.
     try {
@@ -61,11 +63,12 @@ object Option {
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
 
-  def variance(xs: Seq[Double]): Option[Double] = mean(xs)map((mean: Double) => xs.map((el: Double) => Math.pow((el - mean), 2)).sum / xs.length)
+  def variance(xs: Seq[Double]): Option[Double] = mean(xs) flatMap ((m: Double) => mean( xs map ((x: Double) => pow(x - m, 2))))
 
   def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
+
 
   def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = sys.error("todo")
 }
@@ -73,6 +76,8 @@ object Option {
 object TestOption {
   def main(args: Array[String]): Unit = {
     val values = List(1.0, 2.0, 3.0, 4.0,5.0)
+    println("mean: " + Option.mean(values))
+    println("squared errors:" + (values map ((x: Double) => Math.pow(x - 3, 2))) )
     println("Testing variance on Sequence: " + values + ": var = " + Option.variance(values))
   }
 
