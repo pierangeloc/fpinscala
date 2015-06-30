9a6480d46b90827e80ee579bbaebeaf521eba047
pierangelo <pierangeloc@gmail.com>
2015-07-01 00:08:24 +0200

    Monoids

--- exercises/src/main/scala/fpinscala/monoids/Monoid.scala
+++ exercises/src/main/scala/fpinscala/monoids/Monoid.scala
@@ -2,7 +2,10 @@ package fpinscala.monoids
 
 import fpinscala.parallelism.Nonblocking._
 import fpinscala.parallelism.Nonblocking.Par.toParOps // infix syntax for `Par.map`, `Par.flatMap`, etc
-
+/**
+ * A Monoid must provide an associative binary operation, and a neutral element
+ * @tparam A
+ */
 trait Monoid[A] {
   def op(a1: A, a2: A): A
   def zero: A
@@ -12,38 +15,44 @@ object Monoid {
 
   val stringMonoid = new Monoid[String] {
     def op(a1: String, a2: String) = a1 + a2
+
     val zero = ""
   }
 
   def listMonoid[A] = new Monoid[List[A]] {
     def op(a1: List[A], a2: List[A]) = a1 ++ a2
+
     val zero = Nil
   }
 
   val intAddition: Monoid[Int] = new Monoid[Int] {
     def op(a1: Int, a2: Int) = a1 + a2
+
     def zero = 0
   }
 
   val intMultiplication: Monoid[Int] = new Monoid[Int] {
     def op(a1: Int, a2: Int) = a1 + a2
+
     def zero = 1
   }
 
   val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
     def op(a1: Boolean, a2: Boolean) = a1 && a2
+
     def zero = true
   }
 
   val booleanAnd: Monoid[Boolean] = new Monoid[Boolean] {
     def op(a1: Boolean, a2: Boolean) = a1 || a2
+
     def zero = false
   }
 
   def optionMonoid[A]: Monoid[Option[A]] = new Monoid[Option[A]] {
     def op(a1: Option[A], a2: Option[A]): Option[A] = {
       //return first option if not None, else the second one
-      map2(a1, a2)((x, y) => x) orElse(a2)
+      map2(a1, a2)((x, y) => x) orElse (a2)
       //much simpler to just do:
       a1 orElse a2
     }
@@ -52,79 +61,82 @@ object Monoid {
   }
 
   //let's reimplement the map2 for convenience
-  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = a flatMap((valA: A) => b map ((valB: B) => f(valA, valB)))
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = a flatMap ((valA: A) => b map ((valB: B) => f(valA, valB)))
 
 
   def endoMonoid[A]: Monoid[A => A] = new Monoid[A => A] {
     def op(f1: A => A, f2: A => A): A => A = f1 andThen f2
+
     //zero is the identity function
     def zero = (a: A) => a
   }
 
-  // TODO: Placeholder for `Prop`. Remove once you have implemented the `Prop`
-  // data type from Part 2.
-//  trait Prop {}
+    import fpinscala.testing._
 
-  // TODO: Placeholder for `Gen`. Remove once you have implemented the `Gen`
-  // data type from Part 2.
+    def monoidLaws[A](m: Monoid[A], gen: Gen[A]): Prop = Prop.forAll(Gen.listOfN(3, gen).map {
+      case a :: b :: c :: Nil => (a, b, c)
+    }) {
+      case (x, y, z) => m.op(m.op(x, y), z) == m.op(x, m.op(y, z))
+    } &&
+      Prop.forAll(gen) {
+        case x => m.op(m.zero, x) == m.op(x, m.zero)
+      }
 
-  import fpinscala.testing._
-  import Prop._
+    //given a list and a monoid on A, fold them through the operation defined in A
+    def concatenate[A](as: List[A], m: Monoid[A]): A = as.foldLeft(m.zero)(m.op)
 
-  def monoidLaws[A](m: Monoid[A], gen: Gen[A]): Prop = Prop.forAll(Gen.listOfN(3, gen).map{
-    case a :: b :: c :: Nil => (a, b, c)
-  }) {
-    case (x, y, z) => m.op(m.op(x, y), z) == m.op(x, m.op(y, z))
-  } &&
-  Prop.forAll(gen){
-    case x => m.op(m.zero, x) == m.op(x, m.zero)
-  }
+    def foldMap[A, B](as: List[A], m: Monoid[B])(f: A => B): B = as.foldLeft(m.zero)((b, a) => m.op(b, f(a)))
+    def foldMapRight[A, B](as: List[A], m: Monoid[B])(f: A => B): B = as.foldRight(m.zero)((a, b) => m.op(f(a), b))
 
-  def trimMonoid(s: String): Monoid[String] = sys.error("todo")
+    //foldRight in terms of foldMap
+    def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B = ???
 
-  def concatenate[A](as: List[A], m: Monoid[A]): A =
-    sys.error("todo")
+    //foldLeft in terms of foldMap
+    def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B = ???
 
-  def foldMap[A, B](as: List[A], m: Monoid[B])(f: A => B): B =
-    sys.error("todo")
+    //version of foldMap that splits the sequence in subsequences, and applies the folding separately to the chunks
+    def foldMapV[A, B](as: IndexedSeq[A], m: Monoid[B])(f: A => B): B = {
+      as match {
+        case Nil => m.zero
+        case (a1: A) :: Nil => f(a1)
+        case _ => {
+          val (l, r) = as.splitAt(as.length)
+          m.op(foldMapV(l, m)(f), foldMapV(r, m)(f))
+        }
+      }
+    }
 
-  def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B =
-    sys.error("todo")
+    def ordered(ints: IndexedSeq[Int]): Boolean =
+      sys.error("todo")
 
-  def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B =
-    sys.error("todo")
+    sealed trait WC
 
-  def foldMapV[A, B](as: IndexedSeq[A], m: Monoid[B])(f: A => B): B =
-    sys.error("todo")
+    case class Stub(chars: String) extends WC
 
-  def ordered(ints: IndexedSeq[Int]): Boolean =
-    sys.error("todo")
+    case class Part(lStub: String, words: Int, rStub: String) extends WC
 
-  sealed trait WC
-  case class Stub(chars: String) extends WC
-  case class Part(lStub: String, words: Int, rStub: String) extends WC
+    def par[A](m: Monoid[A]): Monoid[Par[A]] =
+      sys.error("todo")
 
-  def par[A](m: Monoid[A]): Monoid[Par[A]] = 
-    sys.error("todo")
+    def parFoldMap[A, B](v: IndexedSeq[A], m: Monoid[B])(f: A => B): Par[B] =
+      sys.error("todo")
 
-  def parFoldMap[A,B](v: IndexedSeq[A], m: Monoid[B])(f: A => B): Par[B] = 
-    sys.error("todo") 
+    val wcMonoid: Monoid[WC] = sys.error("todo")
 
-  val wcMonoid: Monoid[WC] = sys.error("todo")
+    def count(s: String): Int = sys.error("todo")
 
-  def count(s: String): Int = sys.error("todo")
+    def productMonoid[A, B](A: Monoid[A], B: Monoid[B]): Monoid[(A, B)] =
+      sys.error("todo")
 
-  def productMonoid[A,B](A: Monoid[A], B: Monoid[B]): Monoid[(A, B)] =
-    sys.error("todo")
+    def functionMonoid[A, B](B: Monoid[B]): Monoid[A => B] =
+      sys.error("todo")
 
-  def functionMonoid[A,B](B: Monoid[B]): Monoid[A => B] =
-    sys.error("todo")
+    def mapMergeMonoid[K, V](V: Monoid[V]): Monoid[Map[K, V]] =
+      sys.error("todo")
 
-  def mapMergeMonoid[K,V](V: Monoid[V]): Monoid[Map[K, V]] =
-    sys.error("todo")
+    def bag[A](as: IndexedSeq[A]): Map[A, Int] =
+      sys.error("todo")
 
-  def bag[A](as: IndexedSeq[A]): Map[A, Int] =
-    sys.error("todo")
 }
 
 trait Foldable[F[_]] {
@@ -192,4 +204,5 @@ object OptionFoldable extends Foldable[Option] {
   override def foldRight[A, B](as: Option[A])(z: B)(f: (A, B) => B) =
     sys.error("todo")
 }
+
 
--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -5,16 +5,76 @@ import scala.util.matching.Regex
 import fpinscala.testing._
 import fpinscala.testing.Prop._
 
+trait Parser[A] {
+
+}
+
 trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
 
-  case class ParserOps[A](p: Parser[A]) {
+  def map[A, B](p: Parser[A])(f: A => B): Parser[B]
 
+  def run[A](p: Parser[A])(s: String): Either[ParseError, A]
+
+  def or[A](p1: Parser[A], p2: Parser[A]): Parser[A]
+
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+
+
+  def char(c: Char): Parser[Char]
+
+  def many[A](p: Parser[A]): Parser[List[A]]
+
+
+  // count occurrences of a character, on a String starting with it
+  def counter(c: Char): Parser[Int] = map(many(char(c)))(_.size)
+
+  // count if one or more occurrences, fail if string not starting with 'c'
+  def countOneOrMore(c: Char): Parser[Int]
+
+  //implicit conversion, gives a parser whenever it expects a Parser but finds a String
+  implicit def string(s: String): Parser[String]
+
+  //WTF???
+  implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
+
+  //converts a type A to the equivalent String parser, given a function that gives the String representation of A
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
+
+  //parser operations on a parser
+  case class ParserOps[A](p: Parser[A]) {
+    //this way we define infix operators (they take 1 parameter) in term of prefix operator "or" defined in Parsers trait
+    def | [B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+    def or [B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+    def map[B](f: A => B) = self.map(p)(f)
+    def many = self.many(p)
 
   }
 
   object Laws {
+
+
+    run("alpha")("alphabeta") == Right("alpha")
+
+    run("alpha" | "beta")("alphabeta") == Right("alpha")
+    run("alpha" | "beta")("betaalpha") == Right("beta")
+
+    run(listOfN(3, "ab" | "cad"))("abcadcad") == Right(List("ab", "cad", "cad"))
+    run(listOfN(3, "ab" | "cad"))("ababab") = Right(List("ab", "ab", "ab"))
+    run(listOfN(3, "ab" | "cad"))("cadabab") == Right(List("cad", "ab", "ab"))
+
+
+
   }
 }
+
+object Me extends App {
+  val p: Parsers[Parser[String]] = null
+  import p._
+  "abra" | "cadabra"
+
+}
+
+
 
 case class Location(input: String, offset: Int = 0) {
 
